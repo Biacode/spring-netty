ce1271d25d06d61c330f687ec3164d6f81f03c0a
Arthur Asatryan <biacoder@gmail.com>
2018-01-28 15:06:27 +0400

    Apply #3 changes.

---
+++ src/test/java/org/biacode/hermes/spring/netty/test/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package org.biacode.hermes.spring.netty.test.controller;
+
+import org.biacode.hermes.spring.netty.annotation.NettyController;
+
+/**
+ * Created by Arthur Asatryan.
+ * Date: 1/28/18
+ * Time: 2:49 PM
+ */
+@NettyController
+public class HelloWorldController {
+}
