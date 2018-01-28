287e0775ddc7c5f1302e1701f9373417971d26d9
Arthur Asatryan <biacoder@gmail.com>
2018-01-28 14:34:26 +0400

    Added build.gradle dependencies.

---
+++ src/main/java/org/biacode/hermes/spring/netty/NettyAutoConfiguration.java
@@ -0,0 +1,12 @@
+package org.biacode.hermes.spring.netty;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Arthur Asatryan.
+ * Date: 1/28/18
+ * Time: 2:21 PM
+ */
+@Configuration
+public class NettyAutoConfiguration {
+}
