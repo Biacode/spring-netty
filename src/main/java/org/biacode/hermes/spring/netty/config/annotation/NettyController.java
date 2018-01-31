f17f57e665628bbb123ecf854c582cfafd8a5020
Arthur Asatryan <biacoder@gmail.com>
2018-01-31 17:06:52 +0400

    Fixed issue with component scanning.

--- src/main/java/org/biacode/hermes/spring/netty/config/annotation/NettyController.java
+++ src/main/java/org/biacode/hermes/spring/netty/config/annotation/NettyController.java
@@ -1,5 +1,7 @@
 package org.biacode.hermes.spring.netty.config.annotation;
 
+import org.springframework.stereotype.Component;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -12,5 +14,6 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
+@Component
 public @interface NettyController {
 }
